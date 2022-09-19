891d8b59fddce91d120d96e16c6a5bb5246b5daf
Дмитрий <@DESKTOP-HVG9QKI>
2022-09-19 10:19:22 +0400

    Добавьте файлы проекта.

---
+++ -----/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[25];
+
+            Random rnd = new Random();
+
+            for (int y = 0; y < array.Length; y++)
+            {
+               array[y] = rnd.Next(150);
+                Console.WriteLine(array[y]);
+            }
+            int sum = array.Sum();
+            Console.WriteLine("Сумма = " + sum);
+            
+
+
+        }
+    }
+}
